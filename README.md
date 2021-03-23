3c5b673e4768333be9eaa64e53657fa442e70a89
Elijah Schow <eschow@texasmonthly.com>
2021-03-23 07:20:26 -0500

    Initialize demo repo

---
+++ README.md
@@ -0,0 +1,18 @@
+# Recurly Demo Project
+
+### Setup
+
+To get started, make a copy of `.env.example`, rename it to `.env`, and enter your Recurly public key.
+
+### Running
+
+```
+npm start
+```
+
+### Debugging
+
+The important code is in `/src/App.tsx`.
+
+Pay attention to the console for debug messages. If you don't see them, make
+sure your dev console is set to show ALL log levels.
---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import React, { useRef, useEffect, useState } from "react";
+import { useRecurly, useCheckoutPricing } from "@recurly/react-recurly";
+
+import "./App.css";
+
+type FormValues = {
+  plan: string;
+  postal_code?: string;
+  country: "US";
+};
+
+function App() {
+  // Form State
+  const [values, setValues] = useState<FormValues>({
+    plan: "bbq_club",
+    postal_code: undefined,
+    country: "US",
+  });
+
+  // Pricing State
+  const [{ price, loading }, setCheckoutPricing] = useCheckoutPricing({
+    subscriptions: [{ plan: values.plan }],
+  });
+
+  // Update pricing when the plan changes
+  useEffect(() => {
+    setCheckoutPricing({
+      subscriptions: [{ plan: values.plan }],
+      address: {
+        // QUESTION: Why does the type definition demand `first_name` and `last_name`? They aren't needed AFAIK.
+        first_name: "",
+        last_name: "",
+        country: values?.country,
+        postal_code: values?.postal_code,
+      },
+    });
+  }, [values?.plan, values?.postal_code, values?.country, setCheckoutPricing]);
+
+  return (
+    <main className="App">
+      <div>
+        <select
+          name="plan"
+          onChange={(event) =>
+            setValues({ ...values, plan: event.target.value })
+          }
+        >
+          <option
+            value="digital_monthly"
+            selected={values?.plan == "digital_monthly"}
+          >
+            Digital Monthly
+          </option>
+          <option value="bbq_club" selected={values?.plan == "bbq_club"}>
+            BBQ Club
+          </option>
+        </select>
+      </div>
+
+      <div>
+        <label htmlFor="postal_code">Postal Code</label>
+        <input
+          type="text"
+          name="postal_code"
+          id="postal_code"
+          onChange={(event) =>
+            setValues({ ...values, postal_code: event.target.value })
+          }
+        />
+      </div>
+
+      <div>{loading ? "Loading..." : price?.now?.total}</div>
+    </main>
+  );
+}
+
+export default App;
